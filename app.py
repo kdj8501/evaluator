b073f60db1af21d35549fa4ec96c96723e89324c
kdj8501 <kdj8501@gmail.com>
2025-01-23 12:07:31 +0900

    fix mAP

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 import torch
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
@@ -7,15 +7,18 @@ import evaluate
 import xlsxwriter
 import paligemma
 import resnet
+import yolo, baseiou
 from resnet import Vocabulary
 from transformers import PaliGemmaForConditionalGeneration, PaliGemmaProcessor
+from ultralytics import YOLO
+import time
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 ################################## Thread for testing ##################################
 class WorkerForResnet(QThread):
     sig = pyqtSignal(int)
-    rep = pyqtSignal(list, list)
+    rep = pyqtSignal(list, list, int)
 
     def __init__(self, lines, model, path, vocab):
         super().__init__()
@@ -26,6 +29,10 @@ class WorkerForResnet(QThread):
         self.bleu = evaluate.load("bleu")
         self.rouge = evaluate.load("rouge")
         self.meteor = evaluate.load("meteor")
+        self.flag = False
+
+    def thread_stop(self):
+        self.flag = True
 
     def run(self):
         report = []
@@ -36,6 +43,8 @@ class WorkerForResnet(QThread):
         sum_meteor_score = 0.0
         
         for l in self.lines:
+            if self.flag == True:
+                return
             img = Image.open(self.path + "/Images/" + l.split("\n")[0].split(",")[0])
             answer = l.split("\n")[0].split(",")[1]
             generate = resnet.generate_caption_for_image(img, self.model, self.vocab)
@@ -51,19 +60,19 @@ class WorkerForResnet(QThread):
             sum_rouge_score += rouge
             sum_meteor_score += meteor
 
-            report.append(l.split("\n")[0].split(",")[0] + "," + answer + "," + generate + "," + str(bleu) + "," + str(rouge) + "," + str(meteor))
+            report.append([l.split("\n")[0].split(",")[0], answer, generate, str(bleu), str(rouge), str(meteor)])
             print("[" + str(idx) + "/" + str(total) + "] " + answer + ", " + generate + " | scores b: " + str(bleu) + " r: " + str(rouge) + " m: " + str(meteor))
             self.sig.emit(int(idx / total * 100))
 
         total_avr = [sum_bleu_score / total, sum_rouge_score / total, sum_meteor_score / total]
-        self.rep.emit(report, total_avr)
+        self.rep.emit(report, total_avr, 0)
         print("Total Average Score = b: " + str(total_avr[0]) + " r: " + str(total_avr[1]) + " m: " + str(total_avr[2]))
         del self.model
         torch.cuda.empty_cache()
 
 class WorkerForPali(QThread):
     sig = pyqtSignal(int)
-    rep = pyqtSignal(list, list)
+    rep = pyqtSignal(list, list, int)
 
     def __init__(self, lines, model, processor, path):
         super().__init__()
@@ -74,6 +83,10 @@ class WorkerForPali(QThread):
         self.bleu = evaluate.load("bleu")
         self.rouge = evaluate.load("rouge")
         self.meteor = evaluate.load("meteor")
+        self.flag = False
+
+    def thread_stop(self):
+        self.flag = True
 
     def run(self):
         report = []
@@ -84,6 +97,8 @@ class WorkerForPali(QThread):
         sum_meteor_score = 0.0
         
         for l in self.lines:
+            if self.flag == True:
+                return
             img = self.path + "/Images/" + l.split("\n")[0].split(",")[0]
             answer = l.split("\n")[0].split(",")[1]
             generate = paligemma.get_result_pali(img, "describe en", self.model, self.processor)
@@ -99,13 +114,70 @@ class WorkerForPali(QThread):
             sum_rouge_score += rouge
             sum_meteor_score += meteor
 
-            report.append(l.split("\n")[0].split(",")[0] + "," + answer + "," + generate + "," + str(bleu) + "," + str(rouge) + "," + str(meteor))
+            report.append([l.split("\n")[0].split(",")[0], answer, generate, str(bleu), str(rouge), str(meteor)])
             print("[" + str(idx) + "/" + str(total) + "] " + answer + ", " + generate + " | scores b: " + str(bleu) + " r: " + str(rouge) + " m: " + str(meteor))
             self.sig.emit(int(idx / total * 100))
 
         total_avr = [sum_bleu_score / total, sum_rouge_score / total, sum_meteor_score / total]
-        self.rep.emit(report, total_avr)
+        self.rep.emit(report, total_avr, 0)
         print("Total Average Score = b: " + str(total_avr[0]) + " r: " + str(total_avr[1]) + " m: " + str(total_avr[2]))
+        del self.model
+        torch.cuda.empty_cache()
+
+class WorkerForYolo(QThread):
+    sig = pyqtSignal(int)
+    rep = pyqtSignal(list, list, int)
+
+    def __init__(self, model, path, thres):
+        super().__init__()
+        self.model = model
+        self.path = path
+        self.flag = False
+        self.thres = thres
+
+    def thread_stop(self):
+        self.flag = True
+
+    def run(self):
+        report = []
+        idx = 0
+        sum_accuracy = 0.0
+        sum_precision = 0.0
+        sum_recall = 0.0
+        sum_mAP = 0.0
+        data = os.listdir(self.path + "/images")
+        for i in range(len(data)):
+            data[i] = data[i][:data[i].rfind(".")]
+        total = len(data)
+
+        for d in data:
+            idx += 1
+            f = open(self.path + "/labels/" + d + ".txt", 'r')
+            tmp = f.readlines()
+            f.close()
+            ref = []
+            for t in tmp:
+                tt = t.split(" ")
+                ref.append([int(tt[0]), float(tt[1]), float(tt[2]), float(tt[3]), float(tt[4])])
+            pre = yolo.get_result_yolo(self.path + "/images/" + d + ".jpg", self.model)
+            precision = baseiou.getPrecision(ref, pre, self.thres)
+            recall = baseiou.getRecall(ref, pre, self.thres)
+            acc = baseiou.getAccuracy(ref, pre, self.thres)
+            mAP = baseiou.getmAP(ref, pre, self.thres)
+            sum_accuracy += acc
+            sum_precision += precision
+            sum_recall += recall
+            sum_mAP += mAP
+            report.append([d, precision, recall, acc, mAP])
+            self.sig.emit(int(idx / total * 100))
+
+        total_avr_acc = sum_accuracy / total
+        total_avr_prc = sum_precision / total
+        total_avr_rec = sum_recall / total
+        total_avr_map = sum_mAP / total
+
+        self.rep.emit(report, [total_avr_prc, total_avr_rec, total_avr_acc, total_avr_map], 1)
+        print("Total Average Accuracy = " + str([total_avr_prc, total_avr_rec, total_avr_acc, total_avr_map]))
         del self.model
         torch.cuda.empty_cache()
 
@@ -114,51 +186,74 @@ class MainWindow(QMainWindow):
     excel_path = ""
     data_path = ""
     model_path = ""
+    thres = 0.0
+    sel = 0 # 0: ResNet & LSTM, 1: PaliGemma2, 2: YOLO v11x
 
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Model Evaluator v1.0")
-        self.setGeometry(300, 300, 400, 180)
-        self.setFixedSize(400, 180)
+        self.setWindowTitle("Model Evaluator v1.1")
+        self.setGeometry(300, 300, 400, 160)
+        self.setFixedSize(400, 160)
 
         self.pBar = QProgressBar(self)
         self.pBar.move(20, 20)
         self.pBar.resize(370, 20)
         self.pBar.setValue(0)
-        self.testBtn = QPushButton("테스트 시작", self)
-        self.testBtn.move(150, 55)
-        self.testBtn.clicked.connect(self.test)
 
         self.label = QLabel("Threshold(%)", self)
-        self.label.move(150, 90)
-        self.text = QLineEdit("60", self)
+        self.label.move(80, 55)
+        self.text = QLineEdit("50", self)
         self.text.setFixedSize(30, 20)
-        self.text.move(230, 95)
+        self.text.move(160, 60)
+
+        self.testBtn = QPushButton("테스트 시작", self)
+        self.testBtn.move(220, 55)
+        self.testBtn.clicked.connect(self.test)
+
+        self.radio1 = QRadioButton("ResNet + LSTM", self)
+        self.radio1.move(20, 95)
+        self.radio1.setFixedSize(150, 20)
+        self.radio2 = QRadioButton("PaliGemma", self)
+        self.radio2.move(160, 90)
+        self.radio3 = QRadioButton("YOLO v11x", self)
+        self.radio3.move(280, 90)
+        self.radio3.setChecked(True)
 
         self.excelBtn = QPushButton("결과 출력 위치", self)
-        self.excelBtn.move(10, 140)
+        self.excelBtn.move(10, 120)
         self.excelBtn.clicked.connect(self.excel)
         self.folderBtn = QPushButton("데이터 폴더", self)
-        self.folderBtn.move(150, 140)
+        self.folderBtn.move(150, 120)
         self.folderBtn.clicked.connect(self.data)
-        self.selBtn = QPushButton("모델 선택", self)
-        self.selBtn.move(290, 140)
+        self.selBtn = QPushButton("모델 위치", self)
+        self.selBtn.move(290, 120)
         self.selBtn.clicked.connect(self.model)
 
     def test(self):
+        self.__class__.thres = int(self.text.text()) / 100
         if (self.__class__.data_path == "" or self.__class__.model_path == "" or self.__class__.excel_path == ""):
             return
-        if (self.testBtn.text() == "테스트 시작"):
-            self.testBtn.setText("테스트 중지")
-            f = open(self.__class__.data_path + "/captions.txt", 'r')
-            f.readline()
-            lines = f.readlines()
-            f.close()
-            test = lines[:100]
+        if (self.testBtn.text() == "테스트 중지"):
+            self.worker.thread_stop()
+            self.pBar.setValue(0)
+            self.testBtn.setText("테스트 시작")
+        elif (self.testBtn.text() == "테스트 시작"):
+            if (self.radio1.isChecked()):
+                self.__class__.sel = 0
+            elif (self.radio2.isChecked()):
+                self.__class__.sel = 1
+            elif (self.radio3.isChecked()):
+                self.__class__.sel = 2
 
-            sel = 0 # 0: ResNet & LSTM, 1: PaliGemma2
-            if (sel == 0):
+            self.testBtn.setText("테스트 중지")
+
+            if (self.sel == 0):
+                f = open(self.__class__.data_path + "/captions.txt", 'r')
+                f.readline()
+                lines = f.readlines()
+                f.close()
+                test = lines[:100]
                 self.model = resnet.load_trained_model(self.__class__.model_path + "/final_model.pth", self.__class__.model_path + "/vocab.pkl")
                 self.worker = WorkerForResnet(test, self.model, self.__class__.data_path, self.__class__.model_path + "/vocab.pkl")
                 self.worker.start()
@@ -166,47 +261,77 @@ class MainWindow(QMainWindow):
                 self.worker.rep.connect(self.report)
                 del self.model
                 torch.cuda.empty_cache()
-            elif (sel == 1):
+            elif (self.sel == 1):
+                f = open(self.__class__.data_path + "/captions.txt", 'r')
+                f.readline()
+                lines = f.readlines()
+                f.close()
+                test = lines[:100]
                 self.model = PaliGemmaForConditionalGeneration.from_pretrained(
                     self.__class__.model_path,
                     torch_dtype=torch.bfloat16,
                     local_files_only=True
                 ).to(DEVICE)
                 self.processor = PaliGemmaProcessor.from_pretrained(self.__class__.model_path, local_files_only=True)
-                self.worker2 = WorkerForPali(test, self.model, self.processor, self.__class__.data_path)
-                self.worker2.start()
-                self.worker2.sig.connect(self.process)
-                self.worker2.rep.connect(self.report)
+                self.worker = WorkerForPali(test, self.model, self.processor, self.__class__.data_path)
+                self.worker.start()
+                self.worker.sig.connect(self.process)
+                self.worker.rep.connect(self.report)
+                del self.model
+                torch.cuda.empty_cache()
+            elif (self.sel == 2):
+                self.model = YOLO('yolo11x.pt')
+                self.worker = WorkerForYolo(self.model, self.__class__.data_path, self.__class__.thres)
+                self.worker.start()
+                self.worker.sig.connect(self.process)
+                self.worker.rep.connect(self.report)
                 del self.model
                 torch.cuda.empty_cache()
 
-    def report(self, rep, avr):
-        workbook = xlsxwriter.Workbook(self.__class__.excel_path + '/report.xlsx')
-        worksheet = workbook.add_worksheet()
-        worksheet.write(0, 0, "image_name")
-        worksheet.write(0, 1, "reference")
-        worksheet.write(0, 2, "prediction")
-        worksheet.write(0, 3, "B")
-        worksheet.write(0, 4, "R")
-        worksheet.write(0, 5, "M")
-        row = 1
-        for r in rep:
-            tmp = r.split(",")
-            worksheet.write(row, 0, tmp[0])
-            worksheet.write(row, 1, tmp[1])
-            worksheet.write(row, 2, tmp[2])
-            worksheet.write(row, 3, tmp[3])
-            worksheet.write(row, 4, tmp[4])
-            worksheet.write(row, 5, tmp[5])
-            row += 1
-        worksheet.write(row + 1, 0, "total average score")
-        worksheet.write(row + 2, 0, "B")
-        worksheet.write(row + 2, 1, "R")
-        worksheet.write(row + 2, 2, "M")
-        worksheet.write(row + 3, 0, str(avr[0]))
-        worksheet.write(row + 3, 1, str(avr[1]))
-        worksheet.write(row + 3, 2, str(avr[2]))
-        workbook.close()
+    def report(self, rep, avr, div):
+        if (div == 0):
+            date = time.strftime('%Y_%m_%d_%H_%M_%S')
+            workbook = xlsxwriter.Workbook(self.__class__.excel_path + '/' + date + '_report.xlsx')
+            worksheet = workbook.add_worksheet()
+            worksheet.write(0, 0, "image_name")
+            worksheet.write(0, 1, "reference")
+            worksheet.write(0, 2, "prediction")
+            worksheet.write(0, 3, "B")
+            worksheet.write(0, 4, "R")
+            worksheet.write(0, 5, "M")
+            row = 1
+            for r in rep:
+                for i in range(len(r)):
+                    worksheet.write(row, i, r[i])
+                row += 1
+            worksheet.write(row + 1, 0, "total average score")
+            worksheet.write(row + 2, 0, "B")
+            worksheet.write(row + 2, 1, "R")
+            worksheet.write(row + 2, 2, "M")
+            worksheet.write(row + 3, 0, str(avr[0]))
+            worksheet.write(row + 3, 1, str(avr[1]))
+            worksheet.write(row + 3, 2, str(avr[2]))
+            workbook.close()
+        elif (div == 1):
+            date = time.strftime('%Y_%m_%d_%H_%M_%S')
+            workbook = xlsxwriter.Workbook(self.__class__.excel_path + '/' + date + '_report.xlsx')
+            worksheet = workbook.add_worksheet()
+            worksheet.write(0, 0, "image_name")
+            worksheet.write(0, 1, "precision")
+            worksheet.write(0, 2, "recall")
+            worksheet.write(0, 3, "accuracy")
+            worksheet.write(0, 4, "mAP." + str(int(round(self.__class__.thres, 2) * 100)))
+            row = 1
+            for r in rep:
+                for i in range(len(r)):
+                    worksheet.write(row, i, r[i])
+                row += 1
+            worksheet.write(row + 1, 0, "total average accuracy")
+            worksheet.write(row + 1, 1, str(avr[0]))
+            worksheet.write(row + 1, 2, str(avr[1]))
+            worksheet.write(row + 1, 3, str(avr[2]))
+            worksheet.write(row + 1, 4, str(avr[3]))
+            workbook.close()
 
     def process(self, num):
         self.pBar.setValue(num)
@@ -222,7 +347,7 @@ class MainWindow(QMainWindow):
         self.__class__.data_path = fname
 
     def model(self):
-        fname = QFileDialog.getExistingDirectory(self, '모델 폴더 선택', '')
+        fname = QFileDialog.getExistingDirectory(self, '모델 위치 선택', '')
         self.__class__.model_path = fname
 
 if __name__ == '__main__':
---
+++ baseiou.py
@@ -0,0 +1,250 @@
+
+def getIOU(list1, list2):
+    x1 = list1[1]
+    y1 = list1[2]
+    w1 = list1[3]
+    h1 = list1[4]
+    x2 = x1 + w1
+    y2 = y1 + h1
+
+    x3 = list2[1]
+    y3 = list2[2]
+    w2 = list2[3]
+    h2 = list2[4]
+    x4 = x3 + w2
+    y4 = y3 + h2
+
+    sq1 = (w1 * h1)
+    sq2 = (w2 * h2)
+    maxx = max(x1, x2, x3, x4)
+    minx = min(x1, x2, x3, x4)
+    maxy = max(y1, y2, y3, y4)
+    miny = min(y1, y2, y3, y4)
+    if (maxx - minx > w1 + w2 or maxy - miny > h1 + h2):
+        return 0.0
+    
+    x5 = max(x1, x3)
+    x6 = min(x2, x4)
+    y5 = max(y1, y3)
+    y6 = min(y2, y4)
+    sq3 = (x6 - x5) * (y6 - y5)
+    iou = sq3 / (sq1 + sq2 - sq3)
+    return iou
+
+def getAccuracy(ref, pre, thres):
+    tp = 0
+    fp = 0
+    fn = 0
+    for p in pre:
+        maxIOU = 0.0
+        ref_type = -1
+        pre_type = -1
+        for r in ref:
+            iou = getIOU(r, p)
+            maxIOU = max(maxIOU, iou)
+            if (maxIOU == iou):
+                ref_type = r[0]
+                pre_type = p[0]
+        if (maxIOU > thres):
+            if (ref_type == pre_type):
+                tp += 1
+            else:
+                fp += 1
+    for r in ref:
+        maxIOU = 0.0
+        for p in pre:
+            iou = getIOU(r, p)
+            maxIOU = max(maxIOU, iou)
+        if (maxIOU == 0.0):
+            fn += 1
+    if (tp + fp + fn == 0.0):
+        return 0.0
+    return tp / (tp + fp + fn)
+
+def getPrecision(ref, pre, thres):
+    if (len(pre) == 0.0):
+        return 0.0
+    tp = 0
+    for p in pre:
+        maxIOU = 0.0
+        ref_type = -1
+        pre_type = -1
+        for r in ref:
+            iou = getIOU(r, p)
+            maxIOU = max(maxIOU, iou)
+            if (maxIOU == iou):
+                ref_type = r[0]
+                pre_type = p[0]
+        if (maxIOU > thres):
+            if (ref_type == pre_type):
+                tp += 1
+    return tp / len(pre)
+
+def getRecall(ref, pre, thres):
+    if (len(ref) == 0.0):
+        return 0.0
+    tp = 0
+    for p in pre:
+        maxIOU = 0.0
+        ref_type = -1
+        pre_type = -1
+        for r in ref:
+            iou = getIOU(r, p)
+            maxIOU = max(maxIOU, iou)
+            if (maxIOU == iou):
+                ref_type = r[0]
+                pre_type = p[0]
+        if (maxIOU > thres):
+            if (ref_type == pre_type):
+                tp += 1
+    return tp / len(ref)
+
+def getF1(ref, pre, thres):
+    if ((getPrecision(ref, pre, thres) + getRecall(ref, pre, thres)) == 0.0):
+        return 0.0
+    return 2 * (getPrecision(ref, pre, thres) * getRecall(ref, pre, thres)) / (getPrecision(ref, pre, thres) + getRecall(ref, pre, thres))
+
+def getAP(ref, pre, len, thres):
+    tmp = []
+    tp = 0
+    fp = 0
+    if (len == 0):
+        return 0.0
+    for p in pre:
+        maxIOU = 0.0
+        ref_type = -1
+        pre_type = -1
+        for r in ref:
+            iou = getIOU(p, r)
+            maxIOU = max(maxIOU, iou)
+            if (maxIOU == iou):
+                ref_type = r[0]
+                pre_type = p[0]
+        if (maxIOU > thres):
+            if (ref_type == pre_type):
+                tp += 1
+            else:
+                fp += 1
+        if (tp + fp == 0.0):
+            tmp.append([0.0, 0.0])
+        else:
+            tmp.append([round(tp / (tp + fp), 3), round(tp / len, 1)])
+
+    # print("[precision, recall] by sequential")
+    # print(tmp)
+    ########## Draw PR Graph with 11-point interpolation ##########
+    poiv = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    poin = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for i in tmp:
+        if (i[1] == 0.0):
+            poiv[0] = i[0]
+            poin[0] += 1
+        if (i[1] == 0.1):
+            poiv[1] += i[0]
+            poin[1] += 1
+        if (i[1] == 0.2):
+            poiv[2] += i[0]
+            poin[2] += 1
+        if (i[1] == 0.3):
+            poiv[3] += i[0]
+            poin[3] += 1
+        if (i[1] == 0.4):
+            poiv[4] += i[0]
+            poin[4] += 1
+        if (i[1] == 0.5):
+            poiv[5] += i[0]
+            poin[5] += 1
+        if (i[1] == 0.6):
+            poiv[6] += i[0]
+            poin[6] += 1
+        if (i[1] == 0.7):
+            poiv[7] += i[0]
+            poin[7] += 1
+        if (i[1] == 0.8):
+            poiv[8] += i[0]
+            poin[8] += 1
+        if (i[1] == 0.9):
+            poiv[9] += i[0]
+            poin[9] += 1
+        if (i[1] == 1.0):
+            poiv[10] += i[0]
+            poin[10] += 1
+    apsum = 0.0
+    for x in range(11):
+        avrPre = 0.0
+        postAvrPre = 0.0
+        if (poin[x] != 0):
+            avrPre = poiv[x] / poin[x]
+        for y in range(11 - x - 1):
+            if (poin[y + x + 1] != 0):
+                postAvrPre = max(poiv[y + x + 1] / poin[y + x + 1], postAvrPre)
+        apsum += max(avrPre, postAvrPre)
+    ap = (apsum) / 11
+    return ap
+
+def getmAP(ref, pre, thres):
+    refcls = {}
+    precls = {}
+
+    for r in ref:
+        if (str(r[0]) in refcls):
+            refcls[str(r[0])].append(r)
+        else:
+            refcls[str(r[0])] = []
+            refcls[str(r[0])].append(r)
+
+    for p in pre:
+        if (str(p[0]) in precls):
+            precls[str(p[0])].append(p)
+        else:
+            precls[str(p[0])] = []
+            precls[str(p[0])].append(p)
+
+    ap = []
+    for p in precls:
+        if (p in refcls):
+            ap.append(getAP(ref, precls[p], len(refcls[p]), thres))
+        else:
+            ap.append(0)
+    sumAP = 0
+    for a in ap:
+        sumAP += a
+    mAP = sumAP / len(ap)
+    return mAP
+
+def run():
+    ref = []
+    ref.append([0, 0.740625, 0.7140625, 0.03203125, 0.10625])
+    ref.append([2, 0.3890625, 0.540625, 0.05625, 0.08359375])
+    ref.append([2, 0.51875, 0.74921875, 0.0765625, 0.14140625])
+    ref.append([2, 0.17890625, 0.415625, 0.0328125, 0.04140625])
+    ref.append([2, 0.17109375, 0.36640625, 0.0234375, 0.0296875])
+    ref.append([2, 0.225, 0.31171875, 0.01171875, 0.015625])
+    ref.append([2, 0.09609375, 0.53203125, 0.071875, 0.08671875])
+    ref.append([2, 0.28203125, 0.271875, 0.0078125, 0.01640625])
+    ref.append([7, 0.1078125, 0.43828125, 0.04609375, 0.065625])
+    ref.append([2, 0.21015625, 0.31953125, 0.0125, 0.01953125])
+    ref.append([2, 0.21015625, 0.31953125, 0.0125, 0.01953125])
+    ref.append([2, 0.29921875, 0.27265625, 0.00625, 0.0078125])
+    ref.append([2, 0.1828125, 0.27421875, 0.00625, 0.00859375])
+    ref.append([2, 0.11640625, 0.284375, 0.01953125, 0.015625])
+    pre = []
+    pre.append([2, 0.5197, 0.7490, 0.0814, 0.1436])
+    pre.append([2, 0.3904, 0.5411, 0.0571, 0.0842])
+    pre.append([2, 0.1792, 0.4162, 0.0350, 0.0418])
+    pre.append([7, 0.1076, 0.4394, 0.0485, 0.0671])
+    pre.append([2, 0.1711, 0.3660, 0.0263, 0.0306])
+    pre.append([0, 0.7412, 0.7089, 0.0331, 0.0933])
+    pre.append([2, 0.2101, 0.3214, 0.0155, 0.0195])
+    pre.append([7, 0.0961, 0.5324, 0.0750, 0.0874])
+    pre.append([2, 0.2257, 0.3165, 0.0157, 0.0117])
+    pre.append([2, 0.0961, 0.5322, 0.0755, 0.0875])
+
+    print("accuracy: " + str(getAccuracy(ref, pre, 0.5)))
+    print("precision: " + str(getPrecision(ref, pre, 0.5)))
+    print("recall: " + str(getRecall(ref, pre, 0.5)))
+    print("f1: " + str(getF1(ref, pre, 0.5)))
+    print("mAP: " + str(getmAP(ref, pre, 0.5)))
+    
+if __name__ == '__main__':
+    run()
